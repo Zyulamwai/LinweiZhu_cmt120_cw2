67d356d0b98843f1322a93873d09ab5581db8a72
Zyulamwai <89134699+Zyulamwai@users.noreply.github.com>
2024-01-17 01:15:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -399,4 +399,4 @@ def weather():
         return jsonify({'msg':'failed', 'data':[]})
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True,host="0.0.0.0")
+    app.run(port=5000,debug=True,host="0.0.0.0")
